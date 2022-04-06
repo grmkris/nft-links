c2eb49051a972f0cda6ff229dad11190ef64f3d0
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-04-06 21:15:41 +0200

    fixed some code

--- components/NFTList.tsx
+++ components/NFTList.tsx
@@ -6,20 +6,20 @@ interface NFTListProps {
 
 function NFTList({dummyNFT}) {
   return (
-    <div className="grid w-full grid-cols-1 justify-items-center md:grid-cols-2  2xl:grid-cols-3">
+    <div className="grid  w-full grid-cols-1 justify-items-center  md:grid-cols-2 2xl:grid-cols-3">
       {dummyNFT.map((nft) => (
         <div
           key={nft.title}
-          className="my-8 flex w-full flex-col items-center justify-center space-y-4 rounded-xl bg-slate-200 p-8 transition-all duration-300 hover:scale-110 md:w-64 xl:w-96"
+          className="my-2 flex w-full flex-col items-center justify-center space-y-4 rounded-xl bg-slate-200 p-4 transition-all duration-300 hover:scale-110 md:w-5/6 lg:my-8 lg:p-8 xl:w-96"
         >
           <div>
             <h1 className="text-2xl font-bold text-black">{nft.title}</h1>
           </div>
 
-          <div className="w-full rounded-lg bg-gradient-to-br from-blue-500 via-gray-500 to-rose-400 p-2 sm:w-1/2 md:w-full">
+          <div className="w-3/4 items-center rounded-lg bg-gradient-to-br from-blue-500 via-gray-500 to-rose-400 p-2 sm:w-1/2 md:w-full">
             <img
               src={nft.image}
-              className="w-76 h-72 rounded-lg object-cover"
+              className="h-72 w-full rounded-lg object-cover  md:h-48 xl:h-60 "
               alt={nft.title}
             />
           </div>
