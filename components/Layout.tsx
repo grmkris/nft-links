25de7032af642621cffa589d8baca4b275540e50
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-04-06 21:05:53 +0200

    Change layout file

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavigationBar from "../NavigationBar";
+import NavigationBar from "./NavigationBar";
 
 interface LayoutProps {
   children: React.ReactNode;
--- pages/mint-nft/index.tsx
+++ pages/mint-nft/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Layout from "../../components/layout/Layout";
+import Layout from "../../components/Layout";
 
 function MintNft() {
   return <Layout>Mint nft</Layout>;
