55b6061986e8c03a6899d240ff4f994d9f57ba34
FlorjanKlancar <klancar.florjan99@gmail.com>
2022-04-08 14:42:18 +0200

    removed console log

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ import Layout from "../components/Layout";
 import { RootState } from "../model/storeModel";
 
 function Dashboard() {
-  const session = useSelector((state: RootState) => state.auth.session);
-  console.log("user123", session);
   return <Layout>dashboard</Layout>;
 }
 
