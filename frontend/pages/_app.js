f4af5eb9d119a5edd9e0477cf234c7f1cbe4d813
grmkris <kristjan.grm1@gmail.com>
2022-04-10 11:46:43 +0200

    feat: connect web3 wallet to website

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,8 +3,43 @@ import "../styles/globals.css";
 import { supabase } from "../utils/supabaseClient";
 import AuthComponent from "../components/AuthComponent";
 import { Provider, useDispatch, useSelector } from "react-redux";
+import {chain, defaultChains, InjectedConnector, WagmiProvider} from 'wagmi'
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect'
+import { WalletLinkConnector } from 'wagmi/connectors/walletLink'
 import store from "../store";
 import { authActions } from "../store/auth-slice";
+
+// API key for Ethereum node
+// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
+const infuraId = process.env.INFURA_ID
+
+// Chains for connectors to support
+const chains = defaultChains
+
+// Set up connectors
+const connectors = ({ chainId }) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0]
+  return [
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+    new WalletConnectConnector({
+      options: {
+        infuraId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      options: {
+        appName: 'My wagmi app',
+        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      },
+    }),
+  ]
+}
 
 function MyApp({ Component, pageProps }) {
   const session = useSelector((state) => state.auth.session);
@@ -28,7 +63,9 @@ function MyApp({ Component, pageProps }) {
 function MyAppWithProvider({ Component, pageProps }) {
   return (
     <Provider store={store}>
-      <MyApp Component={Component} pageProps={pageProps} />
+      <WagmiProvider autoConnect connectors={connectors}>
+        <MyApp Component={Component} pageProps={pageProps} />
+      </WagmiProvider>
     </Provider>
   );
 }
