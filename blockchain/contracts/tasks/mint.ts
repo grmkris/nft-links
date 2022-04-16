ea3d5280008ecec105062e16c2a018763307e8d4
grmkris <kristjan.grm1@gmail.com>
2022-04-17 01:42:30 +0200

    refactor: remove unnecessary files

--- blockchain/contracts/tasks/mint.ts
+++ blockchain/contracts/tasks/mint.ts
@@ -15,3 +15,17 @@ task("mint", "Mints NFT with provided metadata URI to the provided address")
   const result = await nftLink.mintNFT(address, metadata);
   console.log(result);
 });
+
+
+task("metadata", "Gets metadata of the NFT with provided id")
+  .addParam("id", "id of the nft")
+  .setAction(async  ({ id }, { ethers, deployments}) => {
+    const { get } = deployments;
+  const contractAddress = (await get("NftLink")).address;
+  const nftLink = await ethers.getContractAt(
+    "NftLink",
+    contractAddress
+  ) as NftLink;
+  const result = await nftLink.tokenURI(id);
+  console.log(result);
+});
