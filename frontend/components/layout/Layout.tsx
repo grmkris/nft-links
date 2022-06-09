189601fc1707f106dd1807590c44f9f801fbb797
grmkris <kristjan.grm1@gmail.com>
2022-06-09 16:17:50 +0200

    fix: remove unused code

--- frontend/components/layout/Layout.tsx
+++ frontend/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import HeaderBar from './navbar/HeaderBar';
 import Menu from './navbar/Menu';
 
@@ -10,13 +10,11 @@ function Layout({ children }: LayoutProps) {
   return (
     <>
       <HeaderBar />
-      <div className='drawer-mobile drawer'>
+      <div className='drawer drawer-mobile'>
         <input id='my-drawer-2' type='checkbox' className='drawer-toggle' />
-        <div className='drawer-content h-screen overflow-y-scroll'>
-          {children}
-        </div>
+        <div className='drawer-content h-screen overflow-y-scroll'>{children}</div>
         <div className='drawer-side'>
-          <label htmlFor='my-drawer-2' className='drawer-overlay'/>
+          <label htmlFor='my-drawer-2' className='drawer-overlay' />
           <ul className='menu w-80 overflow-y-auto bg-base-200 p-4 text-base-content'>
             <Menu />
           </ul>
