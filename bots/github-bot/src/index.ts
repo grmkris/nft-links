65586b88912d5fed96023974abc9d8952edb8123
grmkris <kristjan.grm1@gmail.com>
2022-06-16 23:17:37 +0200

    feat: github application

---
+++ bots/github-bot/src/index.ts
@@ -0,0 +1,25 @@
+import { Probot } from "probot";
+
+export = (app: Probot) => {
+  app.on("issues.opened", async (context) => {
+    const issueComment = context.issue({
+      body: "Thanks for opening this issue!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+
+  app.on("issues.closed", async (context) => {
+    const issueComment = context.issue({
+      body:
+        "Owner " +
+        context.issue().owner +
+        " of this issue is getting an NFT reward, checkout more on: https://www.niftios.xyz/",
+    });
+    await context.octokit.issues.createComment(issueComment);
+  });
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+};
